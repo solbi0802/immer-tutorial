475f09a6059456676a77cb60e1f6edbc811c6723
SolbiShin <sbshin.dev@gmail.com>
2021-11-18 00:23:33 +0900

    immer를 사용한 불변성 유지

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useCallback, useState } from 'react';
+import produce from 'immer';
 
 const App = () => {
   const nextId = useRef(1);
@@ -10,11 +11,11 @@ const App = () => {
 
   const onChange = useCallback(e => {
     const { name, value } = e.target;
-    setForm({
-      ...form,
-      [name]: [value]
-    });
-  }, [form]);
+    setForm(produce(draft => {
+      draft[name] = value;
+    })
+    );
+  }, []);
 
   const onSubmit = useCallback(e => {
     e.preventDefault();
@@ -24,24 +25,24 @@ const App = () => {
       username: form.username
     };
 
-    setData({
-      ...data,
-      array: data.array.concat(info)
-    });
+    setData(produce(draft => {
+      draft.array.push(info);
+    })
+    );
 
     setForm({
       name: '',
       username: ''
     });
     nextId.current += 1;
-  }, [data, form.name, form.username])
+  }, [form.name, form.username])
 
   const onRemove = useCallback(id => {
-    setData({
-      ...data,
-      array: data.array.filter(info => info.id !== id)
+    setData(produce(draft => {
+      draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
     })
-  }, [data])
+    )
+  }, [])
 
   return (
     <div>
